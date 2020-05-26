2683e2a180939d81dbf110d3e131aa93dcb7322e
Sandro Martini <sandro.martini@gmail.com>
2020-05-26 15:10:36 +0200

    add some config files specific for TypeScript related environments (dev); small update in plugin TypeScript types definition

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,5 +8,5 @@ export interface HealthcheckPluginOptions {
   underPressureOptions?: Record<string, any>
 }
 
-declare const healthcheckPlugin: FastifyMiddleware<HealthcheckPluginOptions>;
-export default healthcheckPlugin;
+declare const healthcheckPlugin: FastifyMiddleware<HealthcheckPluginOptions>
+export default healthcheckPlugin
