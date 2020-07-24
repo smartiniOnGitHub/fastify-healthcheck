0724f30ab744bbe64f19f777ed7abc627c98beec
Sandro Martini <sandro.martini@gmail.com>
2020-07-24 17:48:28 +0200

    update copyright year in license

--- example/example.js
+++ example/example.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 the original author or authors.
+ * Copyright 2018-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 
 const fastify = require('fastify')()
 
-// register plugin with all its options shown but disabled (as a sample)
+// register plugin with all its options shown but commented (as a sample)
 fastify.register(require('../src/plugin'), {
   // healthcheckUrl: '/custom-health',
   // healthcheckUrlDisable: true,
