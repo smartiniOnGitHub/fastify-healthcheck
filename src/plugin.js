87bafbf406fda73b5b2c21cc540e47fdcd752734
Sandro Martini <sandro.martini@gmail.com>
2021-02-09 18:54:25 +0100

    fix lint error (by my previous change here)

--- src/plugin.js
+++ src/plugin.js
@@ -69,7 +69,7 @@ function normalHandler (req, reply) {
 }
 
 function normalHandlerWithUptime (req, reply) {
-  reply.code(200).send({...payloadOK, uptime: process.uptime()})
+  reply.code(200).send({ ...payloadOK, uptime: process.uptime() })
 }
 
 function ensureIsString (arg, name) {
