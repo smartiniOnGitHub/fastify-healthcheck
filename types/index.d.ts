09e202bb664c135493962943e149bd924074aaca
Sandro Martini <sandro.martini@gmail.com>
2022-12-12 18:46:32 +0100

    update ts types to be compatible with NodeNext settings (but not used here); move a type test in the test folder

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,15 +1,47 @@
 /// <reference types="node" />
 
 import { UnderPressureOptions } from '@fastify/under-pressure'
+// import { FastifyInstance, FastifyPluginCallback, FastifyPluginOptions, FastifyRequest } from 'fastify'
 import { FastifyPluginCallback } from 'fastify'
 
-export interface FastifyHealthcheckOptions {
-  healthcheckUrl?: string
-  healthcheckUrlDisable?: boolean
-  healthcheckUrlAlwaysFail?: boolean
-  exposeUptime?: boolean
-  underPressureOptions?: UnderPressureOptions
+// note that this plugin does not use FastifyPlugin, but it's good the same here for types
+
+type FastifyHealthcheckPlugin = FastifyPluginCallback<NonNullable<fastifyHealthcheck.FastifyHealthcheckOptions>>
+
+declare namespace fastifyHealthcheck {
+  export interface FastifyHealthcheckOptions {
+  /**
+   * Override the uri for the healthcheck route (default: '/health').
+   */
+    healthcheckUrl?: string
+
+    /**
+     * Disable the healthcheck route (default: false).
+     */
+    healthcheckUrlDisable?: boolean
+
+    /**
+     * Override to always return failure responses, useful to test failure responses (default: false).
+     */
+    healthcheckUrlAlwaysFail?: boolean
+
+    /**
+     * Override to return Node.js process uptime (default: false).
+     */
+    exposeUptime?: boolean
+
+    /**
+     * Override options for under-pressure (default: {}).
+     */
+    underPressureOptions?: UnderPressureOptions
+  }
+
+  export const fastifyHealthcheck: FastifyHealthcheckPlugin
+  export { fastifyHealthcheck as default }
 }
 
-declare const fastifyHealthcheck: FastifyPluginCallback<FastifyHealthcheckOptions>
-export default fastifyHealthcheck
+declare function fastifyHealthcheck(
+  ...params: Parameters<FastifyHealthcheckPlugin>
+): ReturnType<FastifyHealthcheckPlugin>
+
+export = fastifyHealthcheck
--- types/named-import.test-d.ts
+++
@@ -1,6 +0,0 @@
-import fastify from 'fastify'
-import fastifyHealthcheck, { FastifyHealthcheckOptions } from '..'
-
-const app = fastify()
-
-app.register(fastifyHealthcheck)
